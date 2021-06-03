4e5489340204e26e4129fce55af1c8ae31afe5cf
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-06-03 12:01:08 +0300

    create menu button

---
+++ MenuButton/MenuButton.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuButton
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:MenuButton"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:MenuButton;assembly=MenuButton"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:MenuButton/>
+    ///
+    /// </summary>
+    public class MenuButton : ItemsControl
+    {
+        public static readonly DependencyProperty MenuProperty;
+        public static readonly DependencyProperty CommandProperty;
+        public static RoutedEvent EventClick;
+
+        private Button _button;
+
+        static MenuButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MenuButton), new FrameworkPropertyMetadata(typeof(MenuButton)));
+
+            CommandProperty = DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(MenuButton));
+            MenuProperty = DependencyProperty.Register("Menu", typeof(ContextMenu), typeof(MenuButton), new UIPropertyMetadata(null, OnMenuChanged));
+            EventClick = EventManager.RegisterRoutedEvent("Click", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(MenuButton));
+        }
+
+        private static void OnMenuChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event RoutedEventHandler Click
+        {
+            add => AddHandler(EventClick, value);
+            remove => AddHandler(EventClick, value);
+        }
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public ContextMenu Menu
+        {
+            get => (ContextMenu)GetValue(MenuProperty);
+            set => SetValue(MenuProperty, value);
+        }
+    }
+}
